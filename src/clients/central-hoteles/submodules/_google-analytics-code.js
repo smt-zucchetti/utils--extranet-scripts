46b8cc072a82b6ca510e0b656cbf427a6becf79a
developer <stevenmtune@gmail.com>
2023-05-30 15:31:13 -0700

    update

--- src/clients/central-hoteles/submodules/_google-analytics-code.js
+++ src/clients/central-hoteles/submodules/_google-analytics-code.js
@@ -5,6 +5,7 @@ export async function googleAnalyticsCode()
     await utils.populateBeAttributes();
     
     gtag_enabled = false;
+
     (function (i, s, o, g, r, a, m) {
         i['GoogleAnalyticsObject'] = r;
         i[r] = i[r] || function () {
@@ -45,7 +46,11 @@ export async function googleAnalyticsCode()
     await utils.loadScriptAsync('https://www.googletagmanager.com/gtag/js?id=G-J0QBDVD27H');
     
     window.dataLayer = window.dataLayer || [];
-    function gtagJ0QBDVD27H(){dataLayer.push(arguments);}
+    
+    function gtagJ0QBDVD27H()
+    {
+        dataLayer.push(arguments);
+    }
  
     gtagJ0QBDVD27H('js', new Date());
 
@@ -66,7 +71,39 @@ export async function googleAnalyticsCode()
         'page_path': pageValMap[utils.BE_ATTRIBUTES.page]
     });
     
-    
-    
+    if(utils.BE_ATTRIBUTES.page === 'thank_you_page')
+    {
+        gtagJ0QBDVD27H('event', 'purchase', 
+        {
+            event_category: 'ecommerce', 
+            item_brand: BE_ATTRIBUTES.cmWidgetValues.propertyName, 
+            affiliation: 'Vertical Booking',
+            transaction_id: BE_ATTRIBUTES.cmWidgetValues.code,
+            currency: BE_ATTRIBUTES.cmWidgetValues.currency,
+            coupon: BE_ATTRIBUTES.cmWidgetValues.codiceSconto,
+            arrival_date: BE_ATTRIBUTES.cmWidgetValues.startDateNumbers,
+            depart_date: BE_ATTRIBUTES.cmWidgetValues.endDateNumbers,
+            nights_qty: parseInt(BE_ATTRIBUTES.cmWidgetValues.nights),
+            rooms_qty: parseInt(BE_ATTRIBUTES.cmWidgetValues.rooms),
+            adults: parseInt(BE_ATTRIBUTES.cmWidgetValues.adults),
+            children: parseInt(BE_ATTRIBUTES.cmWidgetValues.children),
+            value: parseInt(BE_ATTRIBUTES.cmWidgetValues.total),
+            tax: parseInt(BE_ATTRIBUTES.cmWidgetValues.totalTaxes),
+            items: [...rooms].map((room, idx) => 
+            {
+                return {
+                    item_category: 'Rooms',
+                    item_id: room.dataset['roomCod'], // Codice camera
+                    item_name: room.dataset['roomName'], // Tipo di camera
+                    item_variant: room.dataset['productName'], //Rate code
+                    price: parseInt(room.dataset['total']), // Prezzo medio per notte
+                    //promotion_id: '@@OFFER_CODE@@',
+                    //promotion_name: '@@OFFER_NAME@@',
+                    quantity: parseInt(BE_ATTRIBUTES.cmWidgetValues.nights), // Numero notti
+                    index: ++idx
+                };
+            })
+        })
+    }
 
 }
