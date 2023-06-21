575621c8dfff1636e60230af54adda8016c19081
developer <stevenmtune@gmail.com>
2023-06-21 10:12:40 -0700

    update

--- src/clients/harbor-shores/script.js
+++ src/clients/harbor-shores/script.js
@@ -5,4 +5,5 @@ submodules.variousTextualChanges();
 submodules.hideLocationPropertyInfo(); 
 utils.disableHilightOnNightsSelect();
 submodules.sojernCode();
+utils.gtm4StandardCode('GTM-5GQ2D35');
 utils.googleTagManager('GTM-5GQ2D35');
