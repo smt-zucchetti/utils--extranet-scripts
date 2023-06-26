71422cd118c418c0a63b0d36b788a2c6f4d3c7f1
developer <stevenmtune@gmail.com>
2023-06-26 10:40:50 -0700

    update

--- src/clients/waterfront-hotel/script.js
+++ src/clients/waterfront-hotel/script.js
@@ -2,5 +2,5 @@ import * as utils from './../../lib/utils';
 import * as submodules from './submodules/main';
 
 submodules.changeResultsPageCancellationPolicyText();
-//utils.gtm4StandardCode('GTM-K5Z7CBH');
-utils.googleTagManager('GTM-K5Z7CBH');
+utils.gtm4StandardCode('GTM-K5Z7CBH');
+//utils.googleTagManager('GTM-K5Z7CBH');
