3b344a874c6b5c77c0ca5bf253faa4b924e0ebea
developer <stevenmtune@gmail.com>
2023-06-26 14:34:55 -0700

    update

--- src/clients/waterfront-hotel/script.js
+++ src/clients/waterfront-hotel/script.js
@@ -2,6 +2,6 @@ import * as utils from "./../../lib/utils";
 import * as submodules from "./submodules/main";
 
 submodules.changeResultsPageCancellationPolicyText();
-utils.gtm4StandardCodeWaterfront("GTM-K5Z7CBH");
+utils.gtm4StandardCode("GTM-K5Z7CBH");
 utils.disableHilightOnNightsSelect();
 utils.stickyFooter();
