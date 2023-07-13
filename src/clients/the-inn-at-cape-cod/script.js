8efc9fced4a4fdce7b1bb64f7128942b2dbd3c60
developer <stevenmtune@gmail.com>
2023-07-13 11:07:12 -0700

    update

--- src/clients/the-inn-at-cape-cod/script.js
+++ src/clients/the-inn-at-cape-cod/script.js
@@ -2,3 +2,4 @@ import * as utils from './../../lib/utils';
 
 utils.googleTagManager('GTM-M6XZFZ7');
 utils.wihpCodeNew('210372','700261042','XCMbCLX-xbMBELLF9M0C');
+utils.excludingTaxesAndFeesText();
